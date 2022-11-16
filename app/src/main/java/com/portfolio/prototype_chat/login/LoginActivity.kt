64122ccf340e6be13856838e29b813675deae9bf
toshio-nakayama <toshio.h1990@gmail.com>
2022-11-16 15:09:54 +0900

    2022/11/16 15:07

---
+++ app/src/main/java/com/portfolio/prototype_chat/login/LoginActivity.kt
@@ -0,0 +1,82 @@
+package com.portfolio.prototype_chat.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.basgeekball.awesomevalidation.utility.RegexTemplate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.portfolio.prototype_chat.MainActivity
+import com.portfolio.prototype_chat.R
+import com.portfolio.prototype_chat.ToastGenerator
+import com.portfolio.prototype_chat.databinding.ActivityLoginBinding
+import com.portfolio.prototype_chat.signup.SignUpActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var validation: AwesomeValidation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        validation = AwesomeValidation(ValidationStyle.BASIC)
+        validation.also { v ->
+            v.addValidation(
+                this,
+                R.id.edit_text_email,
+                Patterns.EMAIL_ADDRESS,
+                R.string.invalid_email
+            )
+            v.addValidation(
+                this,
+                R.id.edit_text_email,
+                RegexTemplate.NOT_EMPTY,
+                R.string.empty_email
+            )
+            v.addValidation(
+                this,
+                R.id.edit_text_password,
+                RegexTemplate.NOT_EMPTY,
+                R.string.empty_password
+            )
+        }
+        binding.buttonLogin.setOnClickListener {
+            if (validation.validate()) {
+                login()
+            }
+        }
+        binding.textViewSignUp.setOnClickListener {
+            startActivity(
+                Intent(
+                    this@LoginActivity,
+                    SignUpActivity::class.java
+                )
+            )
+        }
+    }
+
+    private fun login() {
+        val email = binding.editTextEmail.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                startActivity(
+                    Intent(
+                        this@LoginActivity,
+                        MainActivity::class.java
+                    )
+                )
+            }
+            .addOnFailureListener {
+                ToastGenerator.Builder(applicationContext).resId(R.string.login_failure).build();
+            }
+    }
+}
---
+++ app/src/main/java/com/portfolio/prototype_chat/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.portfolio.prototype_chat.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.portfolio.prototype_chat.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
