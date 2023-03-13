b65807f3c06cfddf780558892f055bcdad589dd3
Toshio <toshio.h1990@gmail.com>
2023-03-13 14:34:19 +0900

    2022/3/13 14:34

--- app/src/main/java/com/portfolio/prototype_chat/activities/LoginActivity.kt
+++ app/src/main/java/com/portfolio/prototype_chat/activities/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.portfolio.prototype_chat.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.basgeekball.awesomevalidation.AwesomeValidation
 import com.basgeekball.awesomevalidation.ValidationStyle
@@ -17,8 +18,8 @@ import com.portfolio.prototype_chat.utils.connectionAvailable
 
 class LoginActivity : AppCompatActivity() {
     
-    private lateinit var auth: FirebaseAuth
-    private val validation: AwesomeValidation = AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT)
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val validation = AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT)
     private lateinit var binding: ActivityLoginBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +27,26 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        auth = Firebase.auth
+        firebaseAuth = Firebase.auth
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         addValidationToViews()
-        binding.buttonLogin.setOnClickListener { mightLogin() }
+        
         binding.buttonSignup.setOnClickListener {
             val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
             startActivity(intent)
         }
+        
+        binding.buttonLogin.setOnClickListener {
+            if (validation.validate()) {
+                if (!connectionAvailable(applicationContext)) {
+                    ToastGenerator.Builder(applicationContext).resId(R.string.offline).build()
+                } else {
+                    firebaseLogin()
+                }
+            }
+        }
+        
     }
     
     override fun onStart() {
@@ -47,32 +59,27 @@ class LoginActivity : AppCompatActivity() {
         }
     }
     
+    private fun firebaseLogin() {
+        binding.progressbarLayout.progressbar.visibility = View.VISIBLE
+        val email = binding.editEmail.text.toString().trim()
+        val password = binding.editPassword.text.toString().trim()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                binding.progressbarLayout.progressbar.visibility = View.GONE
+                ToastGenerator.Builder(applicationContext).resId(R.string.login_failure).build()
+            }
+        
+    }
+    
     private fun addValidationToViews() {
         validation.also { v ->
             v.addValidation(this, R.id.textinput_email, Patterns.EMAIL_ADDRESS, R.string.invalid_email)
             v.addValidation(this, R.id.textinput_email, RegexTemplate.NOT_EMPTY, R.string.empty_email)
             v.addValidation(this, R.id.textinput_password, RegexTemplate.NOT_EMPTY, R.string.empty_password)
-        }
-    }
-    
-    private fun mightLogin() {
-        if (!connectionAvailable(applicationContext)) {
-            ToastGenerator.Builder(applicationContext).resId(R.string.offline).build()
-        } else {
-            if (validation.validate()) {
-                login()
-            }
-        }
-    }
-    
-    private fun login() {
-        val email = binding.editEmail.text.toString().trim()
-        val password = binding.editPassword.text.toString().trim()
-        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
-            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(intent)
-        }.addOnFailureListener {
-            ToastGenerator.Builder(applicationContext).resId(R.string.login_failure).build()
         }
     }
     
