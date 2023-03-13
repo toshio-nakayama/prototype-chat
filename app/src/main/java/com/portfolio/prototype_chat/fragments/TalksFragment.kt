ce08fc4a30edb61cf3258141f5abb477f9d712d5
Toshio <toshio.h1990@gmail.com>
2023-03-13 16:40:45 +0900

    2022/3/13 16:40

--- app/src/main/java/com/portfolio/prototype_chat/fragments/TalksFragment.kt
+++ app/src/main/java/com/portfolio/prototype_chat/fragments/TalksFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.portfolio.prototype_chat.databinding.FragmentTalksBinding
+import com.portfolio.prototype_chat.models.db.Talk
 import com.portfolio.prototype_chat.utils.notifyObserver
 import com.portfolio.prototype_chat.viewmodels.TalksViewModel
 import com.portfolio.prototype_chat.views.adapters.TalksListAdapter
@@ -30,14 +32,10 @@ class TalksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
-        binding.recyclerTalks.layoutManager = LinearLayoutManager(context).apply {
-            reverseLayout = true
-            stackFromEnd = true
-        }
-        val talksListAdapter = TalksListAdapter(requireContext())
-        binding.recyclerTalks.adapter = talksListAdapter
+        binding.shimmerLayout.startShimmer()
+        
         viewModel.talkListLiveData.observe(viewLifecycleOwner) {
-            talksListAdapter.submitList(it)
+            initRecyclerView(it)
         }
         viewModel.talkLiveData.observe(viewLifecycleOwner) {
             viewModel.talkListLiveData.value?.add(it)
@@ -48,6 +46,18 @@ class TalksFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    
+    private fun initRecyclerView(list: List<Talk>) {
+        binding.recyclerTalks.layoutManager = LinearLayoutManager(context).apply {
+            reverseLayout = true
+            stackFromEnd = true
+        }
+        val talksListAdapter = TalksListAdapter(requireContext())
+        talksListAdapter.submitList(list)
+        binding.recyclerTalks.adapter = talksListAdapter
+        binding.shimmerLayout.stopShimmer()
+        binding.shimmerLayout.isVisible = false
     }
 }
 
